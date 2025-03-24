76a0b3771053ef8a085613364aa4d309ed8ca582
thaominh2006 <dothaominh2401@gmail.com>
2025-03-24 22:02:38 +0700

    Add files via upload

---
+++ lesson5.js
@@ -0,0 +1,45 @@
+let employees = JSON.parse(localStorage.getItem("employees")) || [];
+let form = document.getElementById("employeeForm");
+let employeeList = document.getElementById("nhanvien-list");
+function displayEmployees() {
+    employeeList.innerHTML = "";
+    employees.forEach((nhanvien, index) => {
+        let row = document.createElement("tr")
+        row.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${nhanvien.name}</td>
+        <td>${nhanvien.job}</td>
+        `;
+        employeeList.appendChild(row);
+    });
+}
+function saveNhanvien() {
+    localStorage.setItem("employees", JSON.stringify(employees));
+}
+function clearForm() {
+    document.getElementById("name").value = "";
+    document.getElementById("job").value = "";
+}
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let name = document.getElementById("name").value
+    let job = document.getElementById("job").value
+    if (!name) {
+        alert("Ten nhap khong hop le!")
+        return;
+    }
+    if (!job) {
+        alert("Chuc vu khong hop le!")
+        return;
+    }
+    let newEmploy = {
+        name: name,
+        job: job
+    };
+    employees.push(newEmploy);
+    alert("Them nhan vien thanh cong!")
+    saveNhanvien();
+    displayEmployees();
+    clearForm();
+});
+displayEmployees();
